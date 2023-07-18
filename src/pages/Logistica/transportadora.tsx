036efd1d3df1aa7521f8aaa16c2d53cbb11b8eb5
Robson Moraes <112979330+robsoncmoraes@users.noreply.github.com>
2023-07-18 18:29:05 -0300

    Atualizado o designer
    Corrigido erro no CSS

--- src/pages/Logistica/transportadora.tsx
+++ src/pages/Logistica/transportadora.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
+import styles from "./Transportadora.module.css";
+import { UilPlus } from "@iconscout/react-unicons";
+import { UilSearch } from "@iconscout/react-unicons";
 
-export default function transportadora() {
+export default function Transportadora() {
   return (
-    <div>
-      <h1>TRANSPORTADORA</h1>
-    </div>
+    <>
+      <div className={styles.top}>
+        <h1 className={styles.title}>Transportadoras</h1>
+        <button className={styles.btn}>
+          <UilPlus />
+          Cadastrar Transportadora
+        </button>
+      </div>
+      <div className={styles.divBusca}>
+        <UilSearch color="#a3a3a5" align="center" />
+        <input
+          type="text"
+          className={styles.txtBusca}
+          placeholder="Pesquisar por Razão Social ou CNPJ"
+        />
+      </div>
+    </>
   );
 }
